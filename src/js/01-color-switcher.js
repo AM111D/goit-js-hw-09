d308d27932bd4e0d5c1be11f2365419e45073b22
AM111D <Dimasssnike@icloud.com>
2021-12-26 23:59:37 +0200

    add 1,2,3 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop');
+
+btnStart.addEventListener('click', onBtnStart);
+btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStart() {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    randomBackgColor = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    console.log(btnStart)
+}
+
+function onBtnStop() {
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+    clearInterval(randomBackgColor)
+    console.log(btnStart)
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,62 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+// one by one
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
+
+
+const formEl = document.querySelector('.form')
+
+formEl.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  let delay = Number(e.currentTarget.delay.value);
+  let step = Number(e.currentTarget.step.value);
+  let amount = Number(e.currentTarget.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {useIcon: false});
+        }), delay;
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`,  {useIcon: false});
+        }, delay);
+      });
+    delay += step;
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.6;
+  const objPromise = { position, delay };
+
+  return new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        resolve(objPromise);
+  } else {
+        reject(objPromise);
+  }
+}
+  )};
+
+
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     Notify.success('✅ Fulfilled promise ${position} in ${delay}ms');
+//   })
+//   .catch(({ position, delay }) => {
+//     Notify.failure('❌ Rejected promise ${position} in ${delay}ms');
+//   });
+
+  
