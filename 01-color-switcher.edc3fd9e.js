c34061e8799e79e0b1a79f7190ff10ce17e36b8f
AM111D <86677972+AM111D@users.noreply.github.com>
2023-02-27 04:29:50 +0000

    Deploying to gh-pages from @ AM111D/goit-js-hw-09@07273d2f001edaaf2e3d6dfd87d6bc71141c527d 🚀

---
+++ 01-color-switcher.edc3fd9e.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(n){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,randomBackgColor=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(randomBackgColor)}));
+//# sourceMappingURL=01-color-switcher.edc3fd9e.js.map
